628fabb23c44227fc31af9685664954fd0592420
rhit-fosheejm <64857037+rhit-fosheejm@users.noreply.github.com>
2022-10-16 16:14:28 -0400

    Add files via upload

---
+++ Amazon Scraping/Review_Extractor_Pandas_Output.py
@@ -0,0 +1,139 @@
+
+# Description: Scrapes Reviews (ASIN & product_name & rating & text), stores in dataframe, writes to file for model input
+# Input: review page urls in txt file, one url per line
+# Output: csv of reviews storing a dataframe
+
+# import statements
+#from selectorlib import Extractor
+import requests, time
+#import json 
+from time import sleep
+#import csv
+#from dateutil import parser as dateparser
+from bs4 import BeautifulSoup 
+import pandas as pd
+#from sympy import Product
+#import re
+import sys
+
+### Getter Functions
+# Extracts titles of reviews
+def get_review_titles(soup):
+    Review_Title = soup.findAll("a", {"class" : "a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold"})
+    Name = soup.findAll("div", {"data-hook" : "genome-widget"})
+    Title = []
+    for i in range(0,len(Name)):
+        Title.append(Review_Title[i].get_text())
+    # Remove the '\n' from before and after of every Review Title
+    Title[:] = [i.lstrip('\n').rstrip('\n') for i in Title]
+    return Title  
+
+# Extracts names of reviewers from each review
+def get_reviewer_names(soup):
+    Name = soup.findAll("div", {"data-hook" : "genome-widget"}) 
+    Reviewer = []
+    for i in range(2,len(Name)):
+        Reviewer.append(Name[i].get_text())
+    return Reviewer
+
+# Extracts star rating of each review
+def get_review_ratings(soup):
+    Star_Rating = soup.findAll("i", {"class" : "review-rating"}) 
+    Rating = []
+    for i in range(0, len(soup.findAll("div", {"class" : "a-section celwidget"}))):     
+        Rating.append(Star_Rating[i].get_text())
+    return Rating
+
+# Extracts body of each review
+def get_review_text(soup):
+    Review_Text = soup.findAll("span", {"class" : "review-text"})
+    #Another way to grab the review text:
+    #Review_Text = soup.findAll("span", {"data-hook" : "review-body"}) 
+    Description = []
+    for i in range(0,len(Review_Text)):
+        Description.append(Review_Text[i].get_text())
+    # We will remove the '\n' from before and after of every Review Desciption
+    Description[:] = [i.lstrip('\n').rstrip('\n') for i in Description]
+    #To remove the images from the scraped text
+    Description[:] = [i.replace('The media could not be loaded', '').replace('.\n', '').replace('\n\n\n\n', '') for i in Description]
+    #Strips the text of all extra trailing and leading white space
+    Description[:] = [i.strip() for i in Description]
+
+    return Description
+
+# Extracts date & country of each review
+def get_date_and_country(soup):
+    Review_Info = soup.findAll("span", {"data-hook":"review-date"})
+    Country_And_Date = []
+    for i in range(0,len(Review_Info)):
+        Country_And_Date.append(Review_Info[i].get_text())
+    Country_And_Date[:] = [i.lstrip('Reviewed in the ').split(" on ") for i in Country_And_Date]
+    Country, Date = map(list, zip(*Country_And_Date))
+
+    return Date, Country
+
+# Extracts product_name of each review
+def get_product_name(soup):
+    Product = soup.find("title")
+    Name = []
+    for i in range (0, len(soup.findAll("span", {"class" : "review-text"}))):
+        Name.append(Product.get_text())
+    Name[:] = [i.lstrip('Amazon.com: Customer reviews: ') for i in Name]
+    return Name
+
+# Headers for request library interfacing with browsers
+HEADERS = ({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+                'Accept-Language': 'en-US, en;q=0.5'})
+
+### Main Code
+# input to open statement is txt file with review-page-urls one per line
+with open("CompleteSetOfReviewPages - Copy.txt",'r') as urllist:
+    
+    #create large data frame
+    allReviews = pd.DataFrame()
+    counter = 1
+
+    # vars for ASIN extraction from url
+    substr = "/product-reviews/"
+    len_ASIN = len("B09HTZFH4L")
+
+    # iterate over each url & scrape reviews + populate in interior df to concat exterior df
+    for url in urllist.readlines():
+        #request call for each url in the file
+        webpage = requests.get(url.rstrip("\n"), headers=HEADERS)
+ 
+        # Soup Object containing all data
+        soup = BeautifulSoup(webpage.content, "lxml")
+
+        # string logic extract ASIN from url to store in reviewData df
+        starting_index = url.find(substr) + len(substr)
+        ending_index = starting_index + len_ASIN
+        ASIN_in_list = [url[starting_index:ending_index]] *10
+
+        #create pandas data frame & assign scrapable data - ASIN & product_name & rating & text
+        reviewData = pd.DataFrame(columns=['Product', 'Rating', 'Text', 'Date', 'Country','asin'])
+        product_name_list = get_product_name(soup)
+        product_rating_list = get_review_ratings(soup)
+        product_review_list = get_review_text(soup)
+        product_date_country_list = product_date_list, product_country_list = get_date_and_country(soup)
+        for i in range(len(product_name_list)):
+            #list_row = [product_name_list[i], product_rating_list[i], product_review_list[i], product_date_list[i], ASIN_in_list[i]]
+            list_row = [product_name_list[i], product_rating_list[i], product_review_list[i], product_date_list[i], product_country_list[i], ASIN_in_list[i]]
+            reviewData.loc[len(reviewData)] = list_row 
+
+        #reviewData["Name"] = get_reviewer_names(soup)                          // method yields indexError due to scraping too few names & not is used in models
+        #reviewData["Title"] = get_review_titles(soup)                          // method yields indexError due to scraping too few titles & not is used in models
+
+        # display reviews & scraping progress
+        print(" ------- url #:", counter, " -------\n")
+        if not(reviewData.empty):
+            print(reviewData)
+
+            #updated how to join together dataframes due to pandas version update
+            reviewData.to_csv("PrioritizedData_50_reviews_withDate.csv", mode ='a', index = False, header =False)
+
+            # update url counter for output parsing
+            counter += 1
+        else: 
+
+            sys.exit() 
---
+++ Amazon Scraping/URL_Generator_Beefy.py
@@ -0,0 +1,152 @@
+from bs4 import BeautifulSoup
+import requests, time
+import sys
+
+# Headers for request
+HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+
+# The webpage URL
+#URL = 'https://www.amazon.com/Tools-1954889-Fiberglass-General-Purpose/dp/B01HD6N80W/ref=sr_1_3?crid=3746G1GKZGBTI&keywords=hammer&qid=1654616585&sprefix=hammer%2Caps%2C52&sr=8-3'
+# breakfast intentional link
+#URL = 'https://www.amazon.com/Hamilton-Beach-25475A-Breakfast-Sandwich/dp/B00EI7DPOO/ref=sr_1_1_sspa?keywords=breakfast%2Bsandwich%2Bmaker&qid=1654612462&sprefix=breakfast%2Caps%2C59&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExVDE2RFJJMzJCNEtFJmVuY3J5cHRlZElkPUExMDQ4MzM0MkVFUUs4T1dXODlBWSZlbmNyeXB0ZWRBZElkPUEwNDQxMzA3MVk3VVdJSDY5S1VRSiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1'
+# splitter log 4 review page links (small for testing)
+#URL = "https://www.amazon.com/SuperHandy-Splitter-Electric-Horizontal-Splitting/dp/B09DFQV9FM/ref=cm_cr_arp_d_product_top?ie=UTF8"
+# Random url for testing (blender)
+#URL = 'https://www.amazon.com/Vitamix-A3500-Professional-Grade-Container-Stainless/dp/B09TS1KKHV/ref=sr_1_1_sspa?crid=9GZBUFDK1U89&keywords=blender&qid=1655820740&sprefix=blender%2Caps%2C121&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzUEZFWEQxNDhMNUZVJmVuY3J5cHRlZElkPUEwNDUyMTU0RjBWMThLT1VFMjRGJmVuY3J5cHRlZEFkSWQ9QTAwNzcwMDYyMjE2OVlFMkdFSkhIJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+# Testing url with 1,800 reviews (Large number of pages) (for debugging generator problems with 10+ review pages)
+#URL = 'https://www.amazon.com/Farberware-Dishwasher-Nonstick-Straining-Champagne/dp/B00XADOTDS/ref=sr_1_9?crid=51QFBA2SENEU&keywords=pot&qid=1655824018&sprefix=pot%2Caps%2C259&sr=8-9'
+
+# typically amz.txt, is the /dp/BKJSGOJAIG links in file
+with open('PrioritizedReviewPageURLs.txt', 'r') as urllist: 
+    # time.sleep(2)
+    # url_counter for output parsing
+    url_counter = 1
+    # for loop per url in amz.txt
+    for url in urllist.readlines():
+        
+        print("\n\nread-in url:", url, " ---- url #: ", url_counter)
+        
+        # HTTP Request
+        webpage = requests.get(url, headers=HEADERS)    
+        
+        # Soup Object containing all data
+        soup = BeautifulSoup(webpage.content, "lxml")
+
+        # review link for span class outer moreso
+        Review_Links = soup.find("span", {"class": "cr-widget-FocalReviews"})
+
+        # scraping attempt boolean var
+        scraping_success = True
+
+        if Review_Links == None:
+            # find href link from entire html
+            Review_Links = soup.find("a", {"class":"a-link-emphasis a-text-bold"})
+
+            # if specific scrape for exact part of html doesn't work
+            if Review_Links == None:
+                # review link outmost dp class (noting dp in link for amz scout)
+                Review_Links = soup.find("div", {"id": "dp"})
+
+                # if outermost fails
+                if Review_Links == None:
+                    print("\n |||||||| THIS LINK FAILED ALL THREE HTML SCRAPING ATTEMPTS ||||||||")
+                    scraping_success = False
+                else:
+                    # review link for span class outer moreso
+                    Review_Links = Review_Links.find("span", {"class": "cr-widget-FocalReviews"})
+                    # find href link inside scraped outer span html
+                    Review_Links = Review_Links.find("a", {"class":"a-link-emphasis a-text-bold"})
+        else:
+            # find href link inside scraped outer span html
+            Review_Links = Review_Links.find("a", {"class":"a-link-emphasis a-text-bold"})
+        
+        if (Review_Links == None):
+            if (scraping_success == True):
+                print("\n ------ No reviews exist for this link ----- \n")
+                url_counter += 1
+                continue
+            else:
+                print("\n ------ scraping failed grab see-all-reviews portion of html even with same format ------ \n")
+                url_counter += 1
+                sys.exit()
+                continue
+        Review_Links = 'https://www.amazon.com'+str(Review_Links).lstrip('[<a data-hook="see-all-reviews-link-foot" class="a-link-emphasis a-text-bold" href="').rstrip('reviewerType=all_reviews">See all reviews</a>').rstrip(';')
+        print("\nLink to review page:", Review_Links, "\n")
+        # end of amz single product scrape
+
+        # # grabs outer div (class) for url for all-reviews page from product page
+        # Review_Links = soup.find("div", {"class":"a-row a-spacing-medium"})
+        # # boolean attempt_2 taken
+        # attempt_2 = False
+        # # check if scraped or not
+        # if Review_Links == None:
+        #     print("\tCouldn't scrape see-all-reviews on product page; class div DNE -- attempt 1")
+        #     # attempt 2 scraping outer div
+        #     Review_Links = soup.find("a", {"data-hook":"see-all-reviews-link-foot"})
+        #     attempt_2 = True
+
+        # if Review_Links == None:
+        #     print("\tCouldn't scrape see-all-reviews on product page; class div DNE -- attempt 2")
+        # else: 
+        #     # grabs hyperlink for see-all-reviews page from class div previously scraped
+        #     if (not(attempt_2)):
+        #         Review_Links = Review_Links.find("a", {"data-hook":"see-all-reviews-link-foot"})
+        #     # check if scraped or not
+        #     if (Review_Links == None and not(attempt_2) == True):
+        #         print("Couldn't scrape hyperlink from scraped class div")
+        #     elif (Review_Links == None and attempt_2 == False):
+        #         print("Couldn't scrape hyperlink from attempt 2")
+
+        # populate urls.txt with all-review pages (org + next-page links)
+        grabbed_all_reviews = False
+        # open urls.txt in write
+        # usually is urls.txt below fyi
+        file = open("BlogNonblogRPURLs.txt", 'a')
+        # write original url to opened file
+        file.write(Review_Links + "\n")
+        # counter for stripped url_links verified page numbers
+        counter = 2
+        # loop through all pages & extract next-page links / append to urls.txt
+        while(not grabbed_all_reviews):
+            # create webpage & soup on current review-page
+            webpage = requests.get(Review_Links, headers=HEADERS)
+            soup = BeautifulSoup(webpage.content, "lxml")
+
+            # grab url for next page
+            Review_Links = soup.find("li", {"class", "a-last"})
+            # reconstruct amazon.com url & strip extra content
+            Review_Links = 'https://www.amazon.com'+str(Review_Links).lstrip('<li class="a-last"><a href="').rstrip('">Next page<span class="a-letter-space"></span><span class="a-letter-space"></span>→</a></li>') # + str(counter)
+            # End loop if review_links size == 39; 39 indicates size of a-disabled a-last html code on final page (no future page to scrape)
+            try:
+                #Changing ; to &
+                temp = list(Review_Links)
+                temp[len(Review_Links)-13] = '&'
+                Review_Links = "".join(temp)
+                # differences - next after btm, getr replaces arp
+                temp = Review_Links.split("cm_cr_arp_d_paging_btm")
+                #To fix links when over 9 pages
+                temp[1] = temp[1].replace('&ageNumber', '&pageNumber').replace(';&', '&')
+                #To fix links when over 99 pages
+                temp[1] = temp[1].replace(';p&geNumber', '&pageNumber')
+                # add getr & next after btm
+                Review_Links = temp[0] + "cm_cr_getr_d_paging_btm_next" + temp[1]
+                # display next_page link if exists
+                print("Next Page Link: ", Review_Links, "\n")
+                #time.sleep(3)
+
+                # write link to file
+                url_to_write = Review_Links + "\n"   # this doesn't work
+                file.write(url_to_write)
+
+                # update url page counter
+                counter += 1
+            except:
+                grabbed_all_reviews = True
+
+        # close file buffer
+        file.close()
+    
+        # increment url_counter
+        url_counter += 1
